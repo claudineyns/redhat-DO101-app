cb505768a76dd082fca3808845075d5058cf0a1f
Claudiney Nascimento <contato@claudiney.dev>
2022-08-28 20:04:13 -0300

    Add '/info' route

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.get('/info', (req, res) => {
+  return res.json({version: '0.0.1-SNAPSHOT'});
+});
+
 app.get('/', (req, res) => {
-  res.send('Welcome!');
+  return res.send('Welcome!');
 });
 
 app.listen(8080, () => console.log('App started on port 8080'));
