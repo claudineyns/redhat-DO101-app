b87f48c1df82bcd92233b3a0e893a2e759091abf
Claudiney Nascimento <contato@claudiney.dev>
2022-08-28 20:48:31 -0300

    Versioning

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/info', (req, res) => {
-  return res.json({version: '0.0.2-SNAPSHOT', ...process.env});
+  return res.json({version: '0.0.4-SNAPSHOT', ...process.env});
 });
 
 app.get('/', (req, res) => {
