4be5f34db34092e014e6045f47f7ab5b4c007ce7
Claudiney Nascimento <contato@claudiney.dev>
2022-08-28 19:46:42 -0300

    Initial version

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Wellcome!');
+});
+
+app.listen(8080, () => console.log('App started on port 8080'));
